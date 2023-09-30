0b25e02cadd971b2b674be0531f27905e54ec4d5
bartosz.molka@gmail.com <74304320+n00sek@users.noreply.github.com>
2023-09-30 21:05:27 +0200

    Types

---
+++ src/schools/entities/enums.entity.ts
@@ -0,0 +1,24 @@
+enum StudyLevel {
+  jednolite_magisterskie = "jednolite magisterskie",
+  drugiego_stopnia = "drugiego stopnia",
+  pierwszego_stopnia = "pierwszego stopnia"
+}
+  
+enum StudyProfile {
+  praktyczny = "praktyczny",
+  og_lnoakademicki = "ogólnoakademicki"
+}
+  
+enum StudyStatus {
+  wygaszane = "wygaszane",
+  zlikwidowane = "zlikwidowane",
+  prowadzone = "prowadzone"
+}
+  
+enum Title {
+  licencjat = "licencjat",
+  in_ynier = "inżynier",
+  magister = "magister",
+  magister_in_ynier = "magister inżynier"
+}
+  
--- src/schools/entities/school.entity.ts
+++ src/schools/entities/school.entity.ts
@@ -1,1 +1,76 @@
-export class School {}
+export { Study, StudyName, RunName, Language, Isced, Institution, Cooperator };
+
+class Study {
+  id?: number;
+  name_id?: number;
+  institution_id?: number;
+  level?: StudyLevel;
+  profile?: StudyProfile;
+  isced_id?: number;
+  create_date?: Date;
+  is_for_teacher?: boolean;
+  language_list?: string;
+  is_shared?: boolean;
+  status?: StudyStatus;
+  expire_date?: string;
+  del_date?: string;
+  trades?: string;
+  cooperator_id?: number;
+  cooperation_start_date?: Date;
+  cooperation_end_date?: Date;
+  run_name_id?: number;
+  run_form?: string;
+  lang_id?: number;
+  run_date?: Date;
+  semester_count?: number;
+  ects_count?: number;
+  is_dual?: boolean;
+  run_status?: StudyStatus;
+  is_work_cooperation?: boolean;
+  run_title?: Title;
+  cooperators?: Cooperator;
+  institutions?: Institution;
+  isced?: Isced;
+  languages?: Language;
+  study_names?: StudyName;
+  run_names?: RunName;
+}
+
+class StudyName {
+  name?: string;
+  id?: number;
+  studies?: Study[];
+}
+
+class RunName {
+  run_name?: string;
+  id?: number;
+  studies?: Study[];
+}
+
+class Language {
+  language?: string;
+  id?: number;
+  studies?: Study[];
+}
+
+class Isced {
+  isced_id?: number;
+  isced_name?: string;
+  id?: number;
+  studies?: Study[];
+}
+
+class Institution {
+  institution?: string;
+  id?: number;
+  studies?: Study[];
+}
+
+class Cooperator {
+  cooperator_id?: string;
+  cooperator_name?: string;
+  is_abroad_cooperator?: boolean;
+  id?: number;
+  studies?: Study[];
+}
