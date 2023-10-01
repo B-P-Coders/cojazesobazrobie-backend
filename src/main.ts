37d233f9e24256b31af10c27555327bdc4421c34
Piotr Nowakowski <piotrek02nowakowski@gmail.com>
2023-10-01 09:31:48 +0200

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:5173' });
+  app.enableCors({ origin: '*' });
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
